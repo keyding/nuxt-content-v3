914ca17b0dc12320ba19a8926a6c7a944983113a
Caven <cavenasdev@gmail.com>
2024-12-18 21:38:06 +0800

    feat: add cloudflare d1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  content: {
+    database: {
+      type: 'd1',
+      binding: 'dcce0bd8-5f1c-403a-acea-6f67d72ff5a3'
+    }
+  }
 })
